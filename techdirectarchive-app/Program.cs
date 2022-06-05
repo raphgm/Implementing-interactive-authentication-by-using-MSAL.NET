0cdd1d876ca23b62ca405fdde7e9bb5b2a00e4c4
Raphael Gab-Momoh <83576746+raphgm@users.noreply.github.com>
2022-06-05 13:55:18 +0100

    Add files via upload
    1.Register an application with the Microsoft identity platform
    
    2.Use the PublicClientApplicationBuilder class in MSAL.NET
    
    3. Acquire a token interactively in a console application

---
+++ techdirectarchive-app/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace techdirectarchiveapp
+{
+    class Program
+    {
+        private const string _clientId = "87421527-0815-43b2-a2ef-bc0a8e90ac8e";
+        private const string _tenantId = "21cef452-6ccc-4171-9420-b34ecb234499";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
+
+
+
+
+
